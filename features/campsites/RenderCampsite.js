6563d45f373794590f4bfe397a61b6419aa4def0
luker369 <walker0369@gmail.com>
2022-08-11 10:15:14 -0500

    ReservationScreen updates

--- features/campsites/RenderCampsite.js
+++ features/campsites/RenderCampsite.js
@@ -1,9 +1,15 @@
 import { useRef } from 'react';
-import { StyleSheet, Text, View, PanResponder, Alert } from 'react-native';
+import {
+    StyleSheet,
+    Text,
+    View,
+    PanResponder,
+    Alert,
+    Share
+} from 'react-native';
 import { Card, Icon } from 'react-native-elements';
 import { baseUrl } from '../../shared/baseUrl';
 import * as Animatable from 'react-native-animatable';
-import CampsiteInfoScreen from '../../screens/CampsiteInfoScreen';
 
 const RenderCampsite = (props) => {
     const { campsite } = props;
@@ -12,7 +18,6 @@ const RenderCampsite = (props) => {
 
     const isLeftSwipe = ({ dx }) => dx < -200;
     const isRightSwipe = ({ dx }) => dx > 200;
-
 
     const panResponder = PanResponder.create({
         onStartShouldSetPanResponder: () => true,
@@ -47,15 +52,24 @@ const RenderCampsite = (props) => {
                     ],
                     { cancelable: false }
                 );
-            }
-            else if (isRightSwipe(gestureState)) {
-                <View>
-                    <CampsiteInfoScreen />
-                </View>
-                
-            }
+            } else if (isRightSwipe(gestureState)) {
+                props.onShowModal();
+             }
         }
     });
+
+    const shareCampsite = (title, message, url) => {
+        Share.share(
+            {
+                title,
+                message: `${title}: ${message} ${url}`,
+                url
+            },
+            {
+                dialogTitle: 'Share ' + title
+            }
+        );
+    };
 
     if (campsite) {
         return (
@@ -93,6 +107,20 @@ const RenderCampsite = (props) => {
                             raised
                             reverse
                             onPress={props.onShowModal}
+                        />
+                        <Icon
+                            name='share'
+                            type='font-awesome'
+                            color='#5637DD'
+                            raised
+                            reverse
+                            onPress={() =>
+                                shareCampsite(
+                                    campsite.name,
+                                    campsite.description,
+                                    baseUrl + campsite.image
+                                )
+                            }
                         />
                     </View>
                 </Card>
